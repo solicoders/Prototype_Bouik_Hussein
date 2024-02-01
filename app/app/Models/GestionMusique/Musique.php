cf21d3dcf81540e28b9dfbe21e823e19d5782b80
husseinbouik <husseinbouik5@gmail.com>
2024-02-01 16:41:08 +0100

    Update dependencies and refactor code

--- app/app/Models/GestionMusique/Musique.php
+++ app/app/Models/GestionMusique/Musique.php
@@ -8,6 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Musique extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id',
+        'titre',
+        'artiste',
+        'type_id',
+        'reference',
+        'created_at',
+        'updated_at',
+    ];
     public function type()
     {
         return $this->belongsTo(Type::class);
--- app/routes/web.php
+++ app/routes/web.php
@@ -12,7 +12,48 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// routes/web.php
 
-Route::get('/', function () {
+use Illuminate\Support\Facades\View;
+use App\Models\GestionMusique\Musique;
+use App\Models\GestionMusique\Type;
+use App\Repository\GestionMusique\MusiquesRepository;
+use Illuminate\Http\Request;
+
+$musiqueRepository = new MusiquesRepository(new Musique());
+
+Route::get('/musiques', function () use ($musiqueRepository) {
+    $musiques = $musiqueRepository->getAllMusiques(); // Replace with the actual method to retrieve all musiques
+    return view('welcome')->with('musiques', $musiques);
+});
+
+Route::get('/musiques/create', function () {
+
     return view('welcome');
+});
+
+Route::post('/musiques', function (Request $request) use ($musiqueRepository) {
+    $musiqueData = $request->all();
+    $musiqueRepository->create($musiqueData); // Replace with the actual method to create a musique
+    return view('welcome')->with('success', 'Musique created successfully');
+});
+
+
+
+Route::get('/musiques/{id}/update', function (Request $request, $id) use ($musiqueRepository) {
+    $musiqueData =[
+        'artiste' =>'test',
+        'titre' =>'test',
+        'reference' =>'test',
+        'type_id' =>1,
+        'created_at' =>'2021-01-01',
+        'updated_at' =>'2021-01-01',
+    ];
+    $musiqueRepository->update($id, $musiqueData); // Replace with the actual method to update a musique
+    return view('welcome')->with('success', 'Musique updated successfully');
+});
+
+Route::delete('/musiques/{id}/delete', function ($id) use ($musiqueRepository) {
+    $musiqueRepository->delete($id); // Replace with the actual method to delete a musique
+    return view('welcome')->with('success', 'Musique deleted successfully');
 });
